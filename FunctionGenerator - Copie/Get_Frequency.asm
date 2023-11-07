db53e893761b2af8a8144079af5b99b04f7f6d49
Steeven-4k <118436212+Steeven-4k@users.noreply.github.com>
2023-11-07 15:39:31 +0100

    Add files via upload
    Function generator - Rectangular wave generator mode 
    Optimisation du code

--- FunctionGenerator - Copie/Get_Frequency.asm
+++ FunctionGenerator - Copie/Get_Frequency.asm
@@ -1,10 +1,10 @@
 #include <p18F4550>
 	
 ; config lines for interrupts        
-CONFIG WDT = OFF    ; disable watchdog timer 
-CONFIG MCLRE = ON   ; MCLEAR Pin On
-CONFIG DEBUG = OFF  ; Disable Debug Mode
-CONFIG FOSC = HS    ; Oscillator Mode 
+CONFIG WDT = OFF			; disable watchdog timer 
+CONFIG MCLRE = ON			; MCLEAR Pin On
+CONFIG DEBUG = OFF			; Disable Debug Mode
+CONFIG FOSC = HS			; Oscillator Mode 
 CONFIG CPUDIV = OSC1_PLL2
 CONFIG PBADEN = OFF
     
@@ -15,15 +15,15 @@ FREQ_MAX equ 1000
  ; declaration of global variables 
 FREQUENCY_POTENTIOMETER equ 0x20
  
-org 0x0000  ; reset vector
+org 0x0000				; reset vector
 goto INIT
-org 0x0008  ; interrupt vector 
+org 0x0008				; interrupt vector 
 goto IRQ_HANDLE
     
 INIT
     ; initialization of PORTA (for the frequency_potentiometer)
-    setf TRISA  ; PORTA is an INPUT
-    clrf PORTA  ; Clear PORTA
+    setf TRISA				; PORTA is an INPUT
+    clrf PORTA				; Clear PORTA
     
     ; initialization of the A/D module
     movlw B'00000000'
@@ -33,55 +33,63 @@ INIT
     bsf ADCON0, ADON
 
     ; configure interrupts 
-    bsf INTCON, GIE	    ; interrupts
-    bsf INTCON, PEIE    ; Periph. Int.
+    bsf INTCON, GIE			; interrupts
+    bsf INTCON, PEIE			; Periph. Int.
     
-    bsf ADCON0, GO_DONE		; start A/D conversion
-return 
+    GOTO MAIN_LOOP
+
+
+MAIN_LOOP
+    ; start the A/D conversion
+    bsf ADCON0, GO_DONE
+    
+    ; Wait for a short delay
+    nop
+    nop
+    
+    GOTO MAIN_LOOP
+END 
     
 ;   --Interrupt routine 
-IRQ_HANDLE		; flag test 
-    btfsc PIR1, ADIF	; is it AD ?
-    goto AD_interrupt	; yes, it is AD
-retfie	; no, return from interrupt
+IRQ_HANDLE				; flag test 
+    btfsc PIR1, ADIF			; Check if the interrupt is from the A/D module
+    GOTO ADInterrupt			; yes, it is AD
+RETFIE					; no, return from interrupt
     
-AD_interrupt 
-    bcf PIR1, ADIF	; clear the A/D interrupt flag
+ADInterrupt 
+    bcf PIR1, ADIF			; clear the A/D interrupt flag
     ; A/D result goes into FREQUENCY_POTENTIOMETER
-    movf ADRESH, W	    ; load the high part of the result
+    movf ADRESH, W			; load the high part of the result
     movf FREQUENCY_POTENTIOMETER	; stock the value in FREQUENCY_POTENTIOMETER
-    bsf ADCON0, GO_DONE 
-retfie 
+    
+    CALL Limit_Frequency
+    bsf ADCON0, GO_DONE			; Start another A/D Conversion
+RETFIE
     
 Limit_Frequency
-    movlw FREQ_MIN	    ; load the min value in WREG
-    subwf FREQUENCY_POTENTIOMETER, W	    ; Substract FREQUENCY_POTENTIOMETER to FREQ_MIN and stock the result in WREG
+    ; Chech if FREQUENCY_POTENTIOMETER is below FREQ_MIN    
+    movlw FREQ_MIN			; load the min value in WREG
+    subwf FREQUENCY_POTENTIOMETER, W	; Substract FREQUENCY_POTENTIOMETER to FREQ_MIN and stock the result in WREG
+    btfss STATUS, Z			; check if result is 0
+    goto Frequency_In_Range		; if the result is positive, FREQUENCY_POTENTIOMETER is in the range
     
-    ; if FREQUENCY_POTENTIOMETER < FREQ_MIN, FREQUENCY_POTENTIOMETER takes the value of FREQ_MIN
-    btfss STATUS, C	    ; check the carry flag
-    goto Frequency_In_Range    ; if the result is positive, FREQUENCY_POTENTIOMETER is in the range
-    
-    movlw FREQ_MIN	    ; load the min value in WREG
-    movwf FREQUENCY_POTENTIOMETER	    ; FREQ_POT takes the value of FREQ_MIN
-    goto Frequency_In_Range
+    ; If below FREQ_MIN, set FREQUENCY_POTENTIOMETER to FREQ_MIN
+    movlw FREQ_MIN	    
+    movwf FREQUENCY_POTENTIOMETER	   
+    goto Frequency_In_Range 
     
 Frequency_In_Range
-    movlw FREQ_MAX	    ; load the max value in WREG
-    subwf FREQUENCY_POTENTIOMETER, W	    ; Substract FREQUENCY_POTENTIOMETER to FREQ_MAX and stock the result in WREG
-
-    ; if FREQUENCY_POTENTIOMETER > FREQ_MAX, FREQUENCY_POTENTIOMETER takes the value of FREQ_MAX
-    btfsc STATUS, C	    ; check the carry flag
+    ; Check if FREQUENCY_POTENTIOMETER is above FREQ_MAX
+    movlw FREQ_MAX			; load the max value in WREG
+    subwf FREQUENCY_POTENTIOMETER, W	; Substract FREQUENCY_POTENTIOMETER to FREQ_MAX and stock the result in WREG
+    btfsc STATUS, C			; check the carry flag
     goto Done
     
-    movlw FREQ_MAX	    ; load FREQ_MAX in WREG
-    movwf FREQUENCY_POTENTIOMETER	    ; FREQUENCY_POTENTIOMETER takes the value of FREQ_MAX
+    ; If above FREQ_MAX, set FREQUENCY_POTENTIOMETER to FREQ_MAX
+    movlw FREQ_MAX			
+    movwf FREQUENCY_POTENTIOMETER	   
  
 Done 
     return
     
-    
-MAIN_LOOP
-    ; start the A/D conversion
-    goto MAIN_LOOP
-    bsf ADCON0, GO_DONE
-END 
+ 
